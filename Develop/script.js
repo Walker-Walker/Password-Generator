03c002414b596c25e219be3ad95a875933cbd47c
Walker-Walker <awalker0@gmail.com>
2020-06-07 20:11:44 -0700

    created an array to contain all character arrays, used that array to cchoose from randomly based on user choice ( using math.random and math.floor) then chose from that randomly chosen array a character , all of this was done once, untill a for loop as created to equal the length of user chosen password length... once for loop iterated these math functions to the desired length it was also storing the randomly chosen element into a variable that would add upon itself using '+=' which could be accesed outside of loop and printed to user.

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,14 @@
 // Assignment code here
 // For each password length, function choose characters randomly( randomCharacter)from Arrays and add to === Password length and return... 
 
-// Get references to the #generate element
+
 var generateBtn = document.querySelector("#generate");
 // Lowercase Array 
-var lowerCaseArray = ["a", "b", "c"];
+var lowerCaseArray = ["a", "b", "c","d","e","f","g","h","i","j","k","l","m","n","o"];
 // Numeric Array 
-var numberArray = [1, 2, 3, 4, 5, 6 ,7];
+var numberArray = [1, 2, 3, 4, 5, 6, 7];
 // Special Character Array - assign numbers to characters using parseInt and const declaration to variable identifier so it cannot be re-assigned 
-var specialCharacterArray = ["!", "@","#","$","%","^","&","*","(",")"];
+var specialCharacterArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
 // Uppercase Array
 var upperCaseArray = ["A", "B", "C"];
@@ -16,28 +16,22 @@ var upperCaseArray = ["A", "B", "C"];
 // Write password to the #password input
 function writePassword() {
   // var password = generatePassword();
-    // if else: if yes run function to choose random lowercase letters and store in "possible" array...
+  // if else: if yes run function to choose random lowercase letters and store in "possible" array...
   // else go to next window.confirm...same for all window confirms...
-  var hasLowercase = window.confirm("do you want lowercase?");
-  if (hasLowercase) {
-    getRandomNumber(lowerCaseArray)
-  }
-  var hasSpecialcharacter = window.confirm("do you want special character?");
-  if (hasSpecialcharacter) {
-    getRandomNumber(specialCharacterArray)
-  }
-  var hasNumber = window.confirm ("do you want a number?");
-  if (hasNumber) {
-    getRandomNumber(numberArray)
-  } 
-  var hasUppercase = window.confirm("do you want an uppercase letter?");
-  if (hasUppercase) {
-    getRandomNumber(upperCaseArray)
-  }
-  var passwordText = document.querySelector("#password");
+
 
   var passwordLength = parseInt(window.prompt("How long do you want your password to be "));
-  console.log(passwordLength);
+  var hasUppercase = window.confirm("do you want an uppercase letter?");
+  var hasNumber = window.confirm("do you want a number?");
+  var hasSpecialcharacter = window.confirm("do you want special character?");
+  var hasLowercase = window.confirm("do you want lowercase?");
+  
+
+  if (hasLowercase === false && hasUppercase === false && hasSpecialcharacter === false && hasNumber === false) {
+    window.alert(" You must choose an option !");
+    writePassword();
+  }
+
   if (passwordLength < 8) {
     window.alert("password length must be 8 characters");
 
@@ -47,189 +41,43 @@ function writePassword() {
     window.alert("password length must be 128 characters");
   }
 
- 
- 
- 
-//   if (passwordLength > 8 || passwordLength < 129) {
-//     //use Mathfloor and Mathrandom to randomly choose characters/ or generate random numeric value that is associated with a character in an array ( math floor rounds math random to whole number mathrandom * 10 creates values from 0 to 9.xx the + 128 will at least give me values to 128 if number is 0)
-    
-//     for (var i = 0; i < passwordLength; i++) {
-      
-    
-//     for (var i = 0; i < 1; i++) {
-     
-//     var randomUpperCaseChoice = Math.floor(Math.random() * upperCaseArray.length);
-//     var choiceUpperCase = upperCaseArray[randomUpperCaseChoice] 
-    
-//   }
-    
-//     for (var i = 0; i < 1; i++) {
-//       var randomLowerCaseChoice = Math.floor(Math.random() * lowerCaseArray.length);
-//       var choiceLowerCase = lowerCaseArray[randomLowerCaseChoice]
-//       console.log(choiceLowerCase)
-
-//     }
-      
-   
-//     for ( var i = 0; i < 1; i++) {
-//       var randomNumberChoice = Math.floor(Math.random() * NumberArray.length);
-//       var choiceNumber = numberArray[randomNumberChoice]
-//       console.log(choiceNumber)
-
-//     }
-    
-
-//     for (var i = 0; i < 1; i++) {
-//       var randomCharacterChoice = Math.floor(Math.random() * specialcharacterArray.length);
-//       var choiceCharacter = specialcharacterArray[randomCharacterChoice]
-//       console.log(choiceCharacter)
-//     }
+  var userCharsetArray = [];
   
-//   break;
-  
-//     }
 
-    
-    
+  if (hasUppercase) {
+    userCharsetArray.push(upperCaseArray)
+  }
 
-// }
+  if (hasLowercase) {
+    userCharsetArray.push(lowerCaseArray)
+  }
 
+  if (hasNumber) {
+    userCharsetArray.push(numberArray)
+  }
+
+  if(hasSpecialcharacter) {
+    userCharsetArray.push(specialCharacterArray)
+  }
+
+  var password =""; //Set Password equal to a string
+ 
+  for (i = 0 ; i < passwordLength; i++) {      // for loop for random math objects to iterate same length of password 
+       var randomNumber = Math.floor(Math.random() *userCharsetArray.length);
+       var randomArrayElement = userCharsetArray[randomNumber];  
+       
+       var n = Math.floor(Math.random()* randomArrayElement.length);
+       var nElement = randomArrayElement[n];     
+      
+        password += nElement                    // password set equal "+=" password = password + nElement --> so after each iteration nElment was added to itself password length amount of times... pass
+  }
+
+     console.log(password);
+     var passwordText = document.querySelector("#password");
+     passwordText.value = password;
 }
 
-// Add event listener to generate button
+
+
 generateBtn.addEventListener("click", writePassword);
 
-//New Proposed layout...
-//activate 1-4 of array functions based on user selection criteria to store random number selctions to empty arrays. if statement returns true then run following get random number function for "array"
-//Length of Password Function and options...
-var userSelection = {
-  length: passwordLength,
-  hasLowercase,
-  hasNumber,
-  hasSpecialcharacter,
-  hasUppercase
-}
-
-
-
-//functions for Random number
-function getRandomNumber (lowerCaseArray) {
-  var randomIndex = Math.floor(Math.random()* lowerCaseArray.length);
-  var randomElement = LowerCaseArray[randomIndex]
-  return randomElement;
-
-}
-
-function getRandomNumber (upperCaseArray) {
-  var randomIndex = Math.floor(Math.random()* upperCaseArray.length);
-  var randomElement = upperCaseArray[randomIndex]
-  return randomElement;
-
-}
-function getRandomNumber (numberArray) {
-  var randomIndex = Math.floor(Math.random()* numberArray.length);
-  var randomElement = numberArray[randomIndex]
-  return randomElement;
-
-}
-function getRandomNumber (specialCharacterArray) {
-  var randomIndex = Math.floor(Math.random()* specialCharacterArray.length);
-  var randomElement = specialCharacterArray[randomIndex]
-  return randomElement;
-
-}
-
-
-var resultPassword = []; 
-
-var possiblePassword = [];
-
-var guaranteedPassword = [];
-
-passwordText.value = [guaranteedPassword];
-
-
-//Uppercase 
-if (userSelection.hasUppercase) {
-   possiblePassword = possiblePassword.concat(upperCaseArray);
-   guaranteedPassword.push(getRandomNumber(upperCaseArray));
-}
-for (let i = 0; i < guaranteedPassword.length; i++) {
-  resultPassword[i] = guaranteedPassword[i];
-}
-//Number 
-if (userSelection.hasNumber) {
-  possiblePassword = possiblePassword.concat(numberArray);
-  guaranteedPassword.push(getRandomNumber(numberArray));
-}
-for (let i = 0; i < guaranteedPassword.length; i++) {
- resultPassword[i] = guaranteedPassword[i];
-}
-
-//LowerCase
- if (userSelection.hasLowercase) {
-  possiblePassword = possiblePassword.concat(lowerCaseArray);
-  guaranteedPassword.push(getRandomNumber(lowerCaseArray));
-}
-for (let i = 0; i < guaranteedPassword.length; i++) {
- resultPassword[i] = guaranteedPassword[i];
-}
-//Specialcharacter
- if (userSelection.hasSpecialcharacter) {
-  possiblePassword = possiblePassword.concat(specialCharacterArray);
-  guaranteedPassword.push(getRandomNumber(specialCharacterArray));
-}
-for (let i = 0; i < guaranteedPassword.length; i++) {
- resultPassword[i] = guaranteedPassword[i];
-}
-
-
-
-
- //Learning Assistant psuedocode...
-//lenght of pass
-//options
-const useroptions = {
-  lenght: passwordLength,
-  hasSpecialcharacter: hasSpecialcharacter,
-
-}
-
-// sepaerate function for rand number
-function getRandomNum(array) {
-  var randomIdex = Math.floor(Math.random() * array.lenght);
-  var randElement = array[randomIdex];
-  return randElement;
-}
-
-
-
-var resultPassword = []; // -> get random 5 elements from possible number array
-
-var possible = []; //-> all 3 array elements
-
-var guaranteed = []; //-> 3 elements 1 special character, 1 number and 1 lower 
-//pass lenght  -5
-//special
-//number
-//lower case letter
-
-if(useroptions.hasSpecialcharacter) {
-  possible = possible.concat(characterArray);
-  guaranteed.push(getRandomNum(characterArray));
-}
-for (let i = 0; i < guaranteed.length; i++) {
- resultPassword[i] = guaranteed[i];
-}
-
-
-
-// function generatePassword() {
-//   var length = 8,
-//       charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-//       retVal = "";
-//   for (var i = 0, n = charset.length; i < length; ++i) {
-//       retVal += charset.charAt(Math.floor(Math.random() * n));
-//   }
-//   return retVal;
-// }
